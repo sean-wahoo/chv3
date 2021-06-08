44e5f107f620a8e01e60f7624c5ef8d780729878
sean <moneyman4949yahoo@gmail.com>
2021-06-07 22:04:45 -0400

    created client-side functions to handle registration validation

--- front/utils/validation.ts
+++ front/utils/validation.ts
@@ -1,3 +1,5 @@
+import Filter from "bad-words";
+
 export function checkPasswordStrength(password: string) {
     const specialCharCheck: RegExp = /[!@#$%^&*(),.?":{}|<>]/g;
     const uppercaseCheck: RegExp = /[A-Z]/g;
@@ -13,9 +15,36 @@ export function checkPasswordStrength(password: string) {
     let checks = [check1, check2, check3, check4, check5];
 
     const passes = checks.filter((check) => check === true);
-    console.log(passes);
 
-    if (passes.length < 2) return { message: "Too weak!" };
-    else if (passes.length < 5) return { message: "Little better..." };
-    else return { message: "Alrighty!" };
+    if (passes.length < 3) return { message: "Too weak!", pass: false };
+    else if (passes.length < 5)
+        return { message: "Little better...", pass: false };
+    else return { message: "Alrighty!", pass: true };
+}
+
+export function checkConfirmPasswordMatchesPassword(
+    password: string,
+    confirmPassword: string
+) {
+    if (password === confirmPassword)
+        return { message: "Matches!", pass: true };
+    else return { message: "Doesn't match!", pass: false };
+}
+
+export function checkThatUsernameIsAllowed(username: string) {
+    if (username.length > 5) {
+        const filter = new Filter();
+        filter.addWords("fucki", "fuki");
+        if (filter.isProfane(username))
+            return { message: "Nice words only!", pass: false };
+        else return { message: "That's better!", pass: true };
+    } else return { message: "Not long enough!", pass: false };
+}
+
+export function checkThatEmailIsAllowed(email: string) {
+    const emailCheck: RegExp =
+        /^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/;
+    if (emailCheck.test(email))
+        return { message: "Correct shape!", pass: true };
+    else return { message: "That's not an email!", pass: false };
 }
