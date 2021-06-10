0c5d0462bf8bfbedd2e8947a86f3807eb01a0b70
sean <moneyman4949yahoo@gmail.com>
2021-06-09 23:52:22 -0400

    added title to url

--- back/routes/posts.ts
+++ back/routes/posts.ts
@@ -2,16 +2,14 @@ import { connection } from "@utils/connection";
 import { nanoid } from "nanoid";
 import { Post, User } from "@utils/interfaces";
 import * as dotenv from "dotenv";
-import * as jwt from "jsonwebtoken";
 
-const SESSION_SECRET = process.env.SESSION_SECRET;
+dotenv.config();
 
 export async function getPosts(req, res) {
     try {
         connection.connect();
         connection.query("SELECT * from posts LIMIT 5", (err, results) => {
             if (err) throw err;
-            console.log(results);
             res.status(200).send(results);
         });
     } catch (error) {
@@ -31,10 +29,13 @@ export async function createPost(req, res) {
             [post_id, user.user_id, post.title, post.content, post.category],
             (err, results) => {
                 if (err) throw err;
-                console.log(results);
+                const urlPostTitle: string = post.title
+                    .toLowerCase()
+                    .split(" ")
+                    .join("-");
                 res.status(200).send({
                     message: "Post created successfully",
-                    link: `${process.env.FRONTEND_URL}/posts/${post_id}`,
+                    link: `${process.env.FRONTEND_URL}/posts/${post_id}/${urlPostTitle}`,
                 });
             }
         );
