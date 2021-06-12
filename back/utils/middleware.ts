5820b5be2ffa8c756968ede3a9ffba2ec3ed5f67
sean <seanreichel.important@gmail.com>
2021-06-12 03:22:06 -0400

    added a friendship middleware to easily grab friendship data for two users on the fly

--- back/utils/middleware.ts
+++ back/utils/middleware.ts
@@ -1,5 +1,7 @@
 import * as dotenv from "dotenv";
 import * as jwt from "jsonwebtoken";
+import { connection } from "@utils/connection";
+import { Friendship } from "@utils/interfaces";
 
 dotenv.config();
 
@@ -15,7 +17,59 @@ export function protectedMiddleware(req, res, next) {
             return res.status(403).send({ error: "jwt failed" });
         }
     } catch (error) {
-        console.log(error);
+        console.error(error);
         return res.status(403).send(error);
+    }
+}
+
+export function friendMiddleware(req, res, next) {
+    try {
+        const logged_in_user_id: string = req.user.user_id;
+        const other_user_id: string = req.query.user_id;
+
+        console.log(logged_in_user_id, other_user_id);
+
+        if (logged_in_user_id !== other_user_id) {
+            connection.connect();
+            connection.query(
+                "SELECT friendship_id, status, send_user_id, recieve_user_id, created_at, updated_at FROM friends WHERE (send_user_id = ? AND recieve_user_id = ?) OR (recieve_user_id = ? AND send_user_id = ?)",
+                [
+                    logged_in_user_id,
+                    other_user_id,
+                    logged_in_user_id,
+                    other_user_id,
+                ],
+                (err, results: any) => {
+                    if (err) throw err;
+                    if (results.length === 0) {
+                        return res
+                            .status(403)
+                            .send({ error: "Friendship not found!" });
+                    }
+                    const friendship: Friendship = {
+                        friendship_id: results[0].friendship_id,
+                        send_user_id: results[0].send_user_id,
+                        recieve_user_id: results[0].recieve_user_id,
+                        logged_in_user_id,
+                        other_user_id,
+                        status: results[0].status,
+                        updated_at: results[0].updated_at,
+                        created_at: results[0].created_at,
+                    };
+
+                    req.friendship = friendship;
+                    next();
+                }
+            );
+        } else {
+            res.status(404).send({
+                error: "Whatever you did was not allowed. Stop!",
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        return res
+            .status(403)
+            .send({ message: "Friendship couldn't be verified!", error });
     }
 }
