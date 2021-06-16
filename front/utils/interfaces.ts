631fa7809c04611b40a03a4cc5b7422a1faf1fbd
sean <seanreichel.important@gmail.com>
2021-06-16 00:02:10 -0400

    added more interfaces to front

--- front/utils/interfaces.ts
+++ front/utils/interfaces.ts
@@ -21,3 +21,58 @@ export interface LoginUser extends User {
     usernameOrEmail: string;
     password: string;
 }
+
+export interface Post {
+    post_id?: string;
+    title: string;
+    content: string;
+    category: string;
+    created_at?: string;
+}
+
+export interface DetailedPost extends Post {
+    num_comments: number;
+    num_likes: number;
+}
+
+export interface Comment {
+    comment_id?: string;
+    user_id: string;
+    post_id: string;
+    is_reply: boolean;
+    reply_id: string | null;
+    content: string;
+    created_at?: string;
+}
+
+export interface DetailedComment extends Comment {
+    num_likes: number;
+    num_replies: number;
+}
+
+export interface Like {
+    like_id?: string;
+    user_id: string;
+    post_id: string | null;
+    comment_id: string | null;
+    created_at: string;
+}
+
+export interface Friendship {
+    friendship_id?: string;
+    send_user_id: string;
+    recieve_user_id: string;
+    logged_in_user_id?: string;
+    other_user_id?: string;
+    status: string;
+    updated_at?: string;
+    created_at?: string;
+}
+
+export interface Message {
+    message_id?: string;
+    send_user_id: string;
+    recieve_user_id: string;
+    content: string;
+    is_read: boolean;
+}
