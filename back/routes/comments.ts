c069cb463b6291e5b3dafdc144605dbb7555349f
sean <seanreichel.important@gmail.com>
2021-06-18 02:20:27 -0400

    created cypress tests for comments and fixed issues presented in routes

--- back/routes/comments.ts
+++ back/routes/comments.ts
@@ -28,35 +28,67 @@ export async function createComment(req, res) {
         connection.connect();
         const user = req.user as User;
         const comment = req.body as Comment;
-        const comment_id = nanoid(12);
-        connection.query(
-            "INSERT INTO comments (comment_id, user_id, post_id, is_reply, reply_id, content) VALUES (?, ?, ?, ?, ?, ?)",
 
-            [
-                comment_id,
-                user.user_id,
-                comment.post_id,
-                comment.is_reply,
-                comment.reply_id,
-                comment.content,
-            ],
-            (err, results) => {
+        if (Object.keys(comment).length < 2) {
+            return res.status(401).send({ error: "Request malformed!" });
+        }
+
+        if (
+            !comment ||
+            !comment.post_id ||
+            !comment.content ||
+            comment.is_reply === null
+        ) {
+            return res
+                .status(401)
+                .send({ error: "Please fill out all fields!" });
+        }
+
+        //! MAKE SURE THE is_reply FIELD GETS CHANGED FROM NULLISH TO FALSEY
+
+        connection.query(
+            "SELECT * from posts WHERE post_id = ?",
+            [comment.post_id],
+            (err, results: any) => {
                 if (err) throw err;
-                if (comment.reply_id === null) {
-                    return res.status(200).send({
-                        message: "Comment created successfully!",
-                    });
-                }
-                connection.query(
-                    "UPDATE comments, (SELECT COUNT(*) AS comment_count FROM comments WHERE reply_id = ?) AS replies SET comments.num_replies = replies.comment_count WHERE comments.comment_id = ?",
-                    [comment.reply_id, comment.reply_id],
-                    (err, results) => {
-                        if (err) throw err;
-                        return res.status(200).send({
-                            message: "Comment created successfully!",
-                        });
-                    }
-                );
+                if (results.length > 0) {
+                    const comment_id = nanoid(12);
+                    connection.query(
+                        "INSERT INTO comments (comment_id, user_id, post_id, is_reply, reply_id, content) VALUES (?, ?, ?, ?, ?, ?)",
+
+                        [
+                            comment_id,
+                            user.user_id,
+                            comment.post_id,
+                            !!comment.is_reply,
+                            comment.reply_id,
+                            comment.content,
+                        ],
+                        (err, results) => {
+                            if (err) throw err;
+                            if (!comment.reply_id) {
+                                comment.comment_id = comment_id;
+                                return res.status(200).send({
+                                    comment_id,
+                                    message: "Comment created successfully!",
+                                });
+                            }
+                            connection.query(
+                                "UPDATE comments, (SELECT COUNT(*) AS comment_count FROM comments WHERE reply_id = ?) AS replies SET comments.num_replies = replies.comment_count WHERE comments.comment_id = ?",
+                                [comment.reply_id, comment.reply_id],
+                                (err, results) => {
+                                    if (err) throw err;
+                                    return res.status(200).send({
+                                        comment_id,
+                                        message:
+                                            "Comment created successfully!",
+                                    });
+                                }
+                            );
+                        }
+                    );
+                } else
+                    return res.status(404).send({ error: "Post not found!" });
             }
         );
     } catch (error) {
@@ -102,5 +134,24 @@ export async function getRepliesToComment(req, res) {
     } catch (error) {
         console.error(error);
         return res.status(500).send(error);
+    }
+}
+
+export async function deleteComment(req, res) {
+    try {
+        const user_id: string = req.user.user_id;
+        const comment_id = req.body.comment.comment_id;
+
+        connection.connect();
+        connection.query(
+            "DELETE FROM comments WHERE comment_id = ? AND user_id = ?",
+            [comment_id, user_id],
+            (err, results) => {
+                if (err) throw err;
+                return res.send({ message: "Comment deleted successfully" });
+            }
+        );
+    } catch (error) {
+        return res.send(error);
     }
 }
