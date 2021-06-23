0a6be3e0af89c338a0706f608aa963b854e91b19
sean <seanreichel.important@gmail.com>
2021-06-23 03:46:01 -0400

    removed comments and added some documentation

--- back/routes/auth.ts
+++ back/routes/auth.ts
@@ -18,16 +18,39 @@ const SESSION_SECRET: string = process.env.SESSION_SECRET;
  */
 export async function registerRoute(req, res) {
     try {
+        /**
+         * @type {number}
+         * used to create password hash
+         */
         const saltRounds = 10;
 
+        /**
+         * @type {mysql.Connection}
+         * connection object for mysql queries
+         */
         const connection = await mysql.createConnection(config);
+
         if (!req.body) return res.status(401).send({ error: "Empty request" });
+
+        /**
+         * @type {User}
+         * user data object to describe request body
+         */
         const user = req.body as RegisterUser;
+
+        /**
+         * @type {object}
+         * dictates what checks the users data has passed or failed
+         */
         const validated = registerValidation(user);
 
         if (validated.passes < 3)
             return res.status(404).send({ error: validated.errors });
 
+        /**
+         * @type {RowDataPacket[] | []}
+         * result from query for users with a given username
+         */
         const [usersWithUsername]: any[] = await connection.execute(
             "SELECT username, email, isGoogle FROM users WHERE username = ?",
             [user.username]
@@ -49,12 +72,29 @@ export async function registerRoute(req, res) {
                 error: "That email is already in use!",
             });
         }
+
+        /**
+         * @type {string}
+         * password hash to take the place of the user's actual password
+         */
         const hash = await bcrypt.hash(user.password, saltRounds);
+
+        /**
+         * @type {string}
+         * unique identifier for user
+         */
         const user_id = nanoid();
         connection.execute(
             "INSERT INTO users (user_id, username, email, password) VALUES (?, ?, ?, ?)",
             [user_id, user.username, user.email, hash]
         );
+
+        /**
+         * @type {string}
+         * session identifying token to be stored on the users browser
+         *
+         * contains user data
+         */
         const token = createSessionToken({
             user_id,
             username: user.username,
@@ -69,75 +109,6 @@ export async function registerRoute(req, res) {
             token: token,
             message: "User successfully registered!",
         });
-
-        // connection.query(
-        //     "SELECT username, email, isGoogle FROM users WHERE username = ?",
-        //     [user.username],
-        //     (error, results: any) => {
-        //         if (error) throw error;
-
-        //         // if (results[0].length > 0) {
-        //         //     if (results[0])
-        //         // }
-
-        //         if (results.length === 0) {
-        //             connection.query(
-        //                 "SELECT email, isGoogle FROM users WHERE email = ?",
-        //                 [user.email],
-        //                 (error, results: any) => {
-        //                     if (error) throw error;
-
-        //                     if (results.length === 0) {
-        //                         bcrypt.hash(
-        //                             user.password,
-        //                             saltRounds,
-        //                             (err, hash) => {
-        //                                 if (err) throw err;
-        //                                 const user_id = nanoid();
-        //                                 connection.query(
-        //                                     "INSERT INTO users (user_id, username, email, password) VALUES (?, ?, ?, ?)",
-        //                                     [
-        //                                         user_id,
-        //                                         user.username,
-        //                                         user.email,
-        //                                         hash,
-        //                                     ],
-        //                                     (err) => {
-        //                                         if (err) throw err;
-        //                                         const token =
-        //                                             createSessionToken({
-        //                                                 user_id,
-        //                                                 username: user.username,
-        //                                                 email: user.email,
-        //                                             });
-        //                                         return res.status(200).send({
-        //                                             user: {
-        //                                                 user_id,
-        //                                                 username: user.username,
-        //                                                 email: user.email,
-        //                                             },
-        //                                             token: token,
-        //                                             message:
-        //                                                 "User successfully registered!",
-        //                                         });
-        //                                     }
-        //                                 );
-        //                             }
-        //                         );
-        //                     } else {
-        //                         return res.status(404).send({
-        //                             error: "That email is already in use!",
-        //                         });
-        //                     }
-        //                 }
-        //             );
-        //         } else {
-        //             return res.status(404).send({
-        //                 error: "That username is already in use!",
-        //             });
-        //         }
-        //     }
-        // );
     } catch (error) {
         console.error(error);
         return res.status(500).send({ error: error.message });
@@ -153,13 +124,26 @@ export async function registerRoute(req, res) {
 export async function loginRoute(req, res) {
     try {
         if (!req.body) return res.status(401).send({ error: "Empty request" });
+
+        /**
+         * @type {User}
+         * user data object to describe request body
+         */
         const user = req.body as LoginUser;
 
+        /**
+         * @type {object}
+         * dictates what checks the users data has passed or failed
+         */
         const validated = loginValidation(user);
 
         if (validated.passes < 1)
             return res.status(404).send({ error: validated.errors });
 
+        /**
+         * @type {mysql.Connection}
+         * connection object for mysql queries
+         */
         const connection = await mysql.createConnection(config);
 
         const [doesUserExist]: any[] = await connection.execute(
@@ -178,6 +162,10 @@ export async function loginRoute(req, res) {
             });
         }
 
+        /**
+         * @type {boolean}
+         * whether or not the user's password is correct
+         */
         const match = await bcrypt.compare(
             user.password,
             doesUserExist[0].password
@@ -186,6 +174,13 @@ export async function loginRoute(req, res) {
         if (!match) {
             return res.status(403).send({ error: "Incorrect password!" });
         }
+
+        /**
+         * @type {string}
+         * session identifying token to be stored on the users browser
+         *
+         * contains user data
+         */
         const token = createSessionToken({
             user_id: doesUserExist[0].user_id,
             username: doesUserExist[0].username,
@@ -200,51 +195,6 @@ export async function loginRoute(req, res) {
             token,
             message: "User logged in successfully!",
         });
-
-        // connection.query(
-        //     "SELECT user_id, username, email, password, isGoogle FROM users WHERE username = ? OR email = ?",
-        //     [user.usernameOrEmail, user.usernameOrEmail],
-        //     (err, results: any) => {
-        //         if (err) throw err;
-        //         if (results.length < 1) {
-        //             return res.status(403).send({
-        //                 error: "User not found!",
-        //             });
-        //         }
-        //         if (results[0].isGoogle === 1) {
-        //             return res.status(403).send({
-        //                 error: "This email already in use!",
-        //             });
-        //         }
-        //         bcrypt.compare(
-        //             user.password,
-        //             results[0].password,
-        //             (err, result) => {
-        //                 if (err) throw err;
-        //                 if (result === true) {
-        //                     const token = createSessionToken({
-        //                         user_id: results[0].user_id,
-        //                         username: results[0].username,
-        //                         email: results[0].email,
-        //                     });
-        //                     res.status(200).send({
-        //                         user: {
-        //                             user_id: results[0].user_id,
-        //                             username: results[0].username,
-        //                             email: results[0].email,
-        //                         },
-        //                         token,
-        //                         message: "User logged in successfully!",
-        //                     });
-        //                 } else {
-        //                     return res
-        //                         .status(403)
-        //                         .send({ error: "Incorrect password!" });
-        //                 }
-        //             }
-        //         );
-        //     }
-        // );
     } catch (error) {
         console.error(error);
         return res.status(500).send({ error: error.message });
@@ -259,12 +209,30 @@ export async function loginRoute(req, res) {
  */
 export async function verifyAuth(req, res) {
     try {
+        /**
+         * @type {string}
+         * secret used by jwt to create a session token and verify it on the server
+         */
         const SESSION_SECRET = process.env.SESSION_SECRET;
 
+        /**
+         * @type {string}
+         * session identifying token to be stored on the users browser
+         *
+         * contains user data
+         */
         const token: string = req.headers.authorization.split("Bearer ")[1];
 
+        /**
+         * @type {string | undefined}
+         * placeholder variable to put the decoded jwt data into
+         */
         let data: any;
         if ((data = jwt.verify(token, SESSION_SECRET))) {
+            /**
+             * @type {mysql.Connection}
+             * connection object for mysql queries
+             */
             const connection = await mysql.createConnection(config);
             const [verifiedUser]: any[] = await connection.execute(
                 "SELECT user_id, username, email FROM users WHERE user_id = ?",
@@ -275,6 +243,10 @@ export async function verifyAuth(req, res) {
                 throw new Error("Not quite sure what went wrong!");
             }
 
+            /**
+             * @type {string}
+             * updated session token with updated user data
+             */
             const newToken = await updateSessionToken(
                 {
                     user_id: verifiedUser[0].user_id,
@@ -284,6 +256,10 @@ export async function verifyAuth(req, res) {
                 token
             );
 
+            /**
+             * @type {object}
+             * data to be returned after verifying a user is authenticated
+             */
             const payload = {
                 isAuth: true,
                 token: newToken,
@@ -294,82 +270,78 @@ export async function verifyAuth(req, res) {
                 },
             };
             return res.send(payload);
-
-            // connection.query(
-            //     "SELECT user_id, username, email FROM users WHERE user_id = ?",
-            //     [data.data.user_id],
-            //     (err, results: any) => {
-            //         if (err) throw err;
-
-            //         if (results.length > 0) {
-            //             updateSessionToken(
-            //                 {
-            //                     user_id: results[0].user_id,
-            //                     username: results[0].username,
-            //                     email: results[0].email,
-            //                 },
-            //                 token,
-            //                 (newToken, err) => {
-            //                     if (err) throw err;
-            //                     const payload = {
-            //                         isAuth: true,
-            //                         token: newToken,
-            //                         user: {
-            //                             user_id: results[0].user_id,
-            //                             username: results[0].username,
-            //                             email: results[0].email,
-            //                         },
-            //                     };
-            //                     return res.send(payload);
-            //                 }
-            //             );
-            //         } else {
-            //             throw new Error("Not quite sure what went wrong!");
-            //         }
-            //     }
-            // );
         } else {
             throw new Error("token_verify_failed");
         }
     } catch (error) {
         console.error(error);
+        // TODO: fix this
         if (error.message === "jwt expired") console.log("it expired");
         return res.send(error);
     }
 }
 
+/**
+ * Route to delete a user
+ * @internal NOT TO BE USED BY NORMAL USERS
+ * @param req Request object
+ * @param res Response object
+ * @returns Response status
+ */
 export async function deleteUser(req, res) {
     try {
+        /**
+         * @type {mysql.Connection}
+         * connection object for mysql queries
+         */
         const connection = await mysql.createConnection(config);
+
+        /**
+         * @type {string}
+         * session identifying token to be stored on the users browser
+         *
+         * contains user data
+         */
         const token: string = req.headers.authorization.split("Bearer ")[1];
 
+        /**
+         * @type {object}
+         * decoded user data from successful token verification
+         */
         const decodedUser: any = jwt.verify(token, SESSION_SECRET);
 
+        /**
+         * @type {string}
+         * user id from {decodedUser} variable
+         */
         const user_id = decodedUser.data.user_id;
         await connection.execute("DELETE FROM users WHERE user_id = ?", [
             user_id,
         ]);
         return res.status(200).send({ message: "User successfully deleted" });
-        // connection.query(
-        //     "DELETE FROM users WHERE user_id = ?",
-        //     [user_id],
-        //     (err, results) => {
-        //         if (err) throw err;
-        //         console.log(user_id);
-        //         return res
-        //             .status(200)
-        //             .send({ message: "User successfully deleted" });
-        //     }
-        // );
     } catch (error) {
         console.error(error);
     }
 }
 
+/**
+ * Route to register or log in a user with Google auth
+ * @param req Request object
+ * @param res Response object
+ * @returns Response status
+ */
 export async function googleSignIn(req, res) {
     try {
+        /**
+         * @type {GoogleUser}
+         * google user data object to describe request body
+         */
         const googleUser = req.body as GoogleUser;
 
+        /**
+         * @type {mysql.Connection}
+         * connection object for mysql queries
+         */
         const connection = await mysql.createConnection(config);
 
         const [doesGoogleUserExist]: any[] = await connection.execute(
@@ -386,6 +358,13 @@ export async function googleSignIn(req, res) {
                 });
             }
             googleUser.user_id = doesGoogleUserExist[0].user_id;
+
+            /**
+             * @type {string}
+             * session identifying token to be stored on the users browser
+             *
+             * contains user data
+             */
             const token = createSessionToken(googleUser);
             return res.send({
                 user: {
@@ -398,11 +377,22 @@ export async function googleSignIn(req, res) {
             });
         }
 
+        /**
+         * @type {string}
+         * uniquely generated user id
+         */
         const user_id: string = nanoid();
         await connection.execute(
             "INSERT INTO users (user_id, username, email, isGoogle) VALUES (?, ?, ?, ?)",
             [user_id, googleUser.username, googleUser.email, true]
         );
+
+        /**
+         * @type {string}
+         * session identifying token to be stored on the users browser
+         *
+         * contains user data
+         */
         googleUser.user_id = user_id;
         const token = createSessionToken(googleUser);
         return res.send({
@@ -414,52 +404,6 @@ export async function googleSignIn(req, res) {
             token,
             message: "User logged in successfully",
         });
-
-        // connection.query(
-        //     "SELECT user_id, email, isGoogle from users WHERE email = ?",
-        //     [googleUser.email],
-        //     (err, results: any) => {
-        //         if (err) throw err;
-        //         if (results.length > 0) {
-        //             if (results[0].isGoogle === 0 && results[0].email !== "") {
-        //                 return res.status(403).send({
-        //                     error: "Email already in use!",
-        //                 });
-        //             }
-        //             googleUser.user_id = results[0].user_id;
-        //             const token = createSessionToken(googleUser);
-        //             return res.send({
-        //                 user: {
-        //                     user_id: results[0].user_id,
-        //                     username: results[0].username,
-        //                     email: results[0].email,
-        //                 },
-        //                 token,
-        //                 message: "User logged in successfully",
-        //             });
-        //         }
-
-        //         const user_id = nanoid();
-        //         connection.query(
-        //             "INSERT INTO users (user_id, username, email, isGoogle) VALUES (?, ?, ?, ?)",
-        //             [user_id, googleUser.username, googleUser.email, 1],
-        //             (err, results) => {
-        //                 if (err) throw err;
-        //                 googleUser.user_id = user_id;
-        //                 const token = createSessionToken(googleUser);
-        //                 return res.send({
-        //                     user: {
-        //                         user_id,
-        //                         username: googleUser.username,
-        //                         email: googleUser.email,
-        //                     },
-        //                     token,
-        //                     message: "User logged in successfully",
-        //                 });
-        //             }
-        //         );
-        //     }
-        // );
     } catch (error) {
         console.error(error);
         return res.send(error);
