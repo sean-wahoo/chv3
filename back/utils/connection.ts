35cb24211f1ac4d7a1c5826f5e15ffb4ddedc09c
sean <seanreichel.important@gmail.com>
2021-06-23 00:42:41 -0400

    changed mysql and import to a bluebird promise based one - added async await to mysql queries :D

--- back/utils/connection.ts
+++ back/utils/connection.ts
@@ -1,14 +1,17 @@
-import * as mysql from "mysql2";
+import * as mysql from "mysql2/promise";
+import bluebird from "bluebird";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-/**
- * mysql database connection object
- */
-export const connection = mysql.createConnection({
+export const config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
-});
+    Promise: bluebird,
+};
+
+/**
+ * mysql database connection object
+ */
